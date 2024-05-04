765af432a05870f6b4abb116fa66d64061f7d54d
feng zhu <happy_admin@163.com>
2024-05-04 22:45:32 +0800

    update

--- src/main/java/com/tutorial/bootdemo/service/DemoService.java
+++
@@ -1,8 +0,0 @@
-package com.tutorial.bootdemo.service;
-
-import org.springframework.stereotype.Service;
-
-public interface DemoService {
-
-    void someMethod();
-}
--- src/main/java/com/tutorial/bootdemo/service/StudentServiceImpl.java
+++ src/main/java/com/tutorial/bootdemo/service/StudentServiceImpl.java
@@ -4,41 +4,22 @@ import com.tutorial.bootdemo.converter.StudentConverter;
 import com.tutorial.bootdemo.dao.Student;
 import com.tutorial.bootdemo.dao.StudentRepository;
 import com.tutorial.bootdemo.dto.StudentDTO;
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
+
 import java.util.List;
 
 @Service
 public class StudentServiceImpl implements StudentService {
-
-    /**
-     Autowired 是 Spring 提供的注解，@Resource 是 JDK 提供的注解。
-     Autowired 默认的注入方式为byType（根据类型进行匹配），
-     Resource 默认注入方式为 byName（根据名称进行匹配）。
-
-     当一个接口只存在一个实现类的情况下，
-     Resource若根据Name无法匹配，会转换成使用byType匹配，
-
-     当一个接口存在多个实现类的情况下，
-     Autowired 和@Resource都需要通过名称才能正确匹配到对应的 Bean。
-     Autowired 可以通过 @Qualifier 注解来显示指定名称，
-     Resource可以通过 name 属性来显示指定名称。
-     * */
-    @Resource
-    @Qualifier("demoServiceImpl1")
-    private DemoService demoService;
 
     @Autowired
     private StudentRepository studentRepository;
 
     @Override
     public StudentDTO getStudentById(long id) {
-        demoService.someMethod();
         Student student = studentRepository.findById(id).orElseThrow(RuntimeException::new);
         return StudentConverter.convertStudent(student);
     }
